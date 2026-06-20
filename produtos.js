d659a165594222fba5214303d15622eed3722b63
Julliotrindade <julliotrindade01@gmail.com>
2026-06-20 15:02:03 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -2,29 +2,32 @@ export const PRODUTOS = [
   {
     nome: "Tradicional",
     descricao: "Pão de batata, salsicha, carne moída, frango, milho e ervilha, batata palha, queijo ralado.",
-    preco: 7
+    preco: 7,
+    imagem: "https://via.placeholder.com/200"
   },
   {
     nome: "Carne de sol na nata",
     descricao: "Pão de batata, salsicha, carne de sol na nata, milho e ervilha, batata palha, queijo ralado.",
-    preco: 9
+    preco: 9,
+    imagem: "https://via.placeholder.com/200"
   },
   {
     nome: "Americano",
     descricao: "Pão de batata, linguiça calabresa, frango, cheddar, milho e ervilha, batata palha, queijo ralado.",
-    preco: 10
+    preco: 10,
+    imagem: "https://via.placeholder.com/200"
   },
   {
     nome: "Refrigerante 1L",
     descricao: "Coca-ZERO, Guaraná e Pepis",
-    preco: 10
-    
+    preco: 10,
+    imagem: "https://via.placeholder.com/200"
   },
   {
     nome: "Refri 250ML",
     descricao: "Soda limão, Uva e Dore-cola.",
-    preco: 3
-   
+    preco: 3,
+    imagem: "https://via.placeholder.com/200"   
   }
   
 ];
